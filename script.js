746f3a81adfe251c61f1a401c9ad59fe57aef974
Quentin Durantay <quentin.durantay@gmail.com>
2016-07-18 11:51:55 +0200

    Stop button now working and reboot timer

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+var interval;
+
 // Begin pomodoro with animation of both timer and loading bar
 $('.fa-play').click(function(event) {
 	$(this).toggleClass('playing');
 	var loading_value = 0;
 	var new_progress = 0;
-	var interval = setInterval(function(){
+	interval = setInterval(function(){
 		loading_value++;
 		new_progress = loading_value * 100 / 25;
 		$('.progress-bar').attr('aria-valuenow', new_progress).css('width', new_progress + '%');
@@ -17,4 +19,11 @@ $('.fa-play').click(function(event) {
 // Ends pomodoro, clears timer and loading bar
 $('.fa-stop').click(function(event) {
 	clearInterval(interval);
+	$(this).toggleClass('stoping');
+	$('.fa-play').removeClass('playing');
+	$('.progress-bar').attr('aria-valuenow', 0).css('width', '0%');
+	$('.timer').text('00');
+	setTimeout(function() {
+		$('.fa-stop').toggleClass('stoping');	
+	}, 50)
 });
