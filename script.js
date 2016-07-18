f2beb0e140c3dc83191b53c630984bf084f86188
Quentin Durantay <quentin.durantay@gmail.com>
2016-07-18 14:05:03 +0200

    Timer now fully working with 25s
    All buttons ok, even when multiple clics

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
 var interval;
+var is_on = false;
 var loading_value = 0;
 var new_progress = 0;
+var paused = false;
+
+function beginTimer() {
+	interval = setInterval(function(){
+			loading_value++;
+			new_progress = loading_value * 100 / 25;
+			$('.progress-bar').attr('aria-valuenow', new_progress).css('width', new_progress + '%');
+			$('.timer').text(loading_value);
+			is_on = true;
+			if (loading_value === 25) {
+				clearInterval(interval);
+				$('.fa-play').removeClass('playing');
+				is_on = false;
+			}
+		}, 1000);
+}
+
+function resetLayout() {
+	clearInterval(interval);
+	is_on = false;
+	$('.fa-play').removeClass('playing');
+	$('.progress-bar').attr('aria-valuenow', 0).css('width', '0%');
+	$('.timer').text('00');
+	loading_value = 0;
+	new_progress = 0;
+}
 
 // Begin pomodoro with animation of both timer and loading bar
 $('.fa-play').click(function(event) {
-	$(this).addClass('playing');
-	interval = setInterval(function(){
-		loading_value++;
-		new_progress = loading_value * 100 / 25;
-		$('.progress-bar').attr('aria-valuenow', new_progress).css('width', new_progress + '%');
-		$('.timer').text(loading_value);
-		if (loading_value === 25) {
-			clearInterval(interval);
-		} 
-	}, 1000)
+	if (is_on === false) {
+		resetLayout();
+		$(this).addClass('playing');
+		beginTimer();
+	} else if (paused === true) {
+		$(this).addClass('playing');
+		beginTimer();
+		paused = false;
+	}
 });
 
 // Pause pomodoro, and restart timer and loading at same level
 $('.fa-pause').click(function(event) {
 	clearInterval(interval);
 	$(this).addClass('pausing');
+	paused = true;
+	$('.fa-play').removeClass('playing');
 	setTimeout(function() {
 		$('.fa-pause').removeClass('pausing');	
 	}, 50);
@@ -27,13 +55,8 @@ $('.fa-pause').click(function(event) {
 
 // Ends pomodoro, clears timer and loading bar
 $('.fa-stop').click(function(event) {
-	clearInterval(interval);
+	resetLayout();
 	$(this).addClass('stoping');
-	$('.fa-play').removeClass('playing');
-	$('.progress-bar').attr('aria-valuenow', 0).css('width', '0%');
-	$('.timer').text('00');
-	loading_value = 0;
-	new_progress = 0;
 	setTimeout(function() {
 		$('.fa-stop').removeClass('stoping');	
 	}, 50);
