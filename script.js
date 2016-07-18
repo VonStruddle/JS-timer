b591b1e02fe27a925515c5624ce0dd80299b9546
Quentin Durantay <quentin.durantay@gmail.com>
2016-07-18 12:11:17 +0200

    Fixed typos and deleted toggleClass for add-remove

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var new_progress = 0;
 
 // Begin pomodoro with animation of both timer and loading bar
 $('.fa-play').click(function(event) {
-	$(this).toggleClass('playing');
+	$(this).addClass('playing');
 	interval = setInterval(function(){
 		loading_value++;
 		new_progress = loading_value * 100 / 25;
@@ -17,23 +17,24 @@ $('.fa-play').click(function(event) {
 });
 
 // Pause pomodoro, and restart timer and loading at same level
-$('fa-pause').click(function(event) {
+$('.fa-pause').click(function(event) {
 	clearInterval(interval);
+	$(this).addClass('pausing');
 	setTimeout(function() {
-		$('.fa-pause').toggleClass('pausing');	
+		$('.fa-pause').removeClass('pausing');	
 	}, 50);
 });
 
 // Ends pomodoro, clears timer and loading bar
 $('.fa-stop').click(function(event) {
 	clearInterval(interval);
-	$(this).toggleClass('stoping');
+	$(this).addClass('stoping');
 	$('.fa-play').removeClass('playing');
 	$('.progress-bar').attr('aria-valuenow', 0).css('width', '0%');
 	$('.timer').text('00');
 	loading_value = 0;
 	new_progress = 0;
 	setTimeout(function() {
-		$('.fa-stop').toggleClass('stoping');	
+		$('.fa-stop').removeClass('stoping');	
 	}, 50);
 });
