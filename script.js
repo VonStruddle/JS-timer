0651688519015b95064027bcd0bc5383822974e1
Quentin Durantay <quentin.durantay@gmail.com>
2016-07-18 14:58:20 +0200

    Now creates modal to know if pomodoro or rest

--- script.js
+++ script.js
@@ -40,6 +40,14 @@ function beginTimer() {
 		}, 1000);
 }
 
+function createBootstrapAlert() {
+	if (max === 25) {
+		$('body').prepend('<div class="alert alert-success"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Get to work! You launched a 25 minutes Pomodoro!</div>');
+	} else {
+		$('body').prepend('<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Time to rest for 5 minutes!</div>');
+	}
+}
+
 function resetLayout() {
 	clearInterval(interval);
 	is_on = false;
@@ -57,6 +65,7 @@ $('.fa-play').click(function(event) {
 		resetLayout();
 		$(this).addClass('playing');
 		beginTimer();
+		createBootstrapAlert();
 	} else if (paused === true) {
 		$(this).addClass('playing');
 		beginTimer();
